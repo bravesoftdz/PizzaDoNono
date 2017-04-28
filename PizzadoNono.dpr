5a29d15d0a7a6caf438f724f94e7eaddbed37abb
Juciane <jucianecarteli@gmail.com>
2017-04-27 23:50:20 -0300

    Pizza do Nono
    Criação dos formulários Base e Principal. Inciado criação de Botões e
    Layout

---
+++ PizzadoNono.dpr
@@ -0,0 +1,16 @@
+program PizzadoNono;
+
+uses
+  Vcl.Forms,
+  uFrmBasePadrao in 'uFrmBasePadrao.pas' {frmBasePadrao},
+  uPrincipal in 'uPrincipal.pas' {frmPrincipal},
+  uFrameBotao in 'uFrameBotao.pas' {FrameBotao: TFrame};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmPrincipal, frmPrincipal);
+  Application.Run;
+end.
---
+++ uPrincipal.pas
@@ -0,0 +1,26 @@
+unit uPrincipal;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, uFrmBasePadrao, Vcl.ComCtrls,
+  Vcl.ToolWin, Vcl.StdCtrls, Vcl.Buttons, Vcl.ExtCtrls, uFrameBotao;
+
+type
+  TfrmPrincipal = class(TfrmBasePadrao)
+    FrameBotao1: TFrameBotao;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmPrincipal: TfrmPrincipal;
+
+implementation
+
+{$R *.dfm}
+
+end.
