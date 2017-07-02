083eca65cfd06b4f3e4d83b56bc1731e27462d46
Ricardo Back <ricardoback1997@gmail.com>
2017-07-02 20:06:35 -0300

    Listando produtos para pedido

--- Pedidos/Produtos/uRegraPedidoProduto.pas
+++ Pedidos/Produtos/uRegraPedidoProduto.pas
@@ -13,7 +13,7 @@ type
   public
     function ValidarDados(var ADtoPedidoProduto: TDtoPedidoProduto): Boolean;
     function Excluir(const AModelPedidoProduto: TModelPedidoProduto; const ADtoPedidoProduto: TDtoPedidoProduto): Boolean;
-
+    function Listar(const AModelPedidoProduto: TModelPedidoProduto): Boolean;
   end;
 
 implementation
@@ -24,6 +24,13 @@ function TRegraPedidoProduto.Excluir(const AModelPedidoProduto: TModelPedidoProd
   const ADtoPedidoProduto: TDtoPedidoProduto): Boolean;
 begin
 
+end;
+
+function TRegraPedidoProduto.Listar(const AModelPedidoProduto: TModelPedidoProduto): Boolean;
+begin
+  Result := False;
+  if AModelPedidoProduto.Listar then
+    Result := True;
 end;
 
 function TRegraPedidoProduto.ValidarDados(var ADtoPedidoProduto: TDtoPedidoProduto): Boolean;
